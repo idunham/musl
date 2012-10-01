0fd4e5daedb3c7c86d16b9247166c1bd62d387c0
Isaac Dunham <idunham@lavabit.com>
2012-10-01 12:04:47 -0700

    More GNU bloat (ABI)

--- src/unistd/access.c
+++ src/unistd/access.c
@@ -5,3 +5,10 @@ int access(const char *filename, int amode)
 {
 	return syscall(SYS_access, filename, amode);
 }
+/*
+ * For glibc compatability.
+ * Not really correct, since they should check effective UID instead.
+ */
+
+weak_alias(access,euidaccess);
+weak_alias(access,eaccess);
