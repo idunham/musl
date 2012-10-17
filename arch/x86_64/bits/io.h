c46912a6db1452cfeb0e0130d471e1a9d7aec834
Isaac Dunham <idunham@lavabit.com>
2012-10-17 15:03:05 -0700

    Revert conditional inline.
    This reverts commit 0b52147f487ce0d33521b1b02669e296ff90448c.
    <features.h> is used now.

--- arch/x86_64/bits/io.h
+++ arch/x86_64/bits/io.h
@@ -31,115 +31,83 @@
 int iopl(int);
 int ioperm(unsigned long, unsigned long, int);
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outb(unsigned char __v, unsigned short __p)
+static inline void outb(unsigned char __v, unsigned short __p)
 {
 	__asm__ volatile ("outb %0,%1" : : "a" (__v), "dN"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outw(unsigned short __v, unsigned short __p)
+static inline void outw(unsigned short __v, unsigned short __p)
 {
 	__asm__ volatile ("outw %0,%1" : : "a" (__v), "dN"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outl(unsigned int __v, unsigned short __p)
+static inline void outl(unsigned int __v, unsigned short __p)
 {
 	__asm__ volatile ("outl %0,%1" : : "a" (__v), "dN"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static unsigned char inb(unsigned short __p)
+static inline unsigned char inb(unsigned short __p)
 {
 	unsigned char __v;
 	__asm__ volatile ("inb %1,%0" : "=a" (__v) : "dN"(__p));
 	return __v;
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static unsigned short inw(unsigned short __p)
+static inline unsigned short inw(unsigned short __p)
 {
 	unsigned short __v;
 	__asm__ volatile ("inw %1,%0" : "=a" (__v) : "dN"(__p));
 	return __v;
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static unsigned int inl(unsigned short __p)
+static inline unsigned int inl(unsigned short __p)
 {
 	unsigned int __v;
 	__asm__ volatile ("inl %1,%0" : "=a" (__v) : "dN"(__p));
 	return __v;
 }
 
+/* String I/O macros */
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outsb(unsigned short __p, const void *__d, unsigned long __n)
+static inline void
+outsb(unsigned short __p, const void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; outsb"
 		      : "+S" (__d), "+c"(__n)
 		      : "d"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outsw(unsigned short __p, const void *__d, unsigned long __n)
+static inline void
+outsw(unsigned short __p, const void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; outsw"
 		      : "+S" (__d), "+c"(__n)
 		      : "d"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void outsl(unsigned short __p, const void *__d, unsigned long __n)
+static inline void
+outsl(unsigned short __p, const void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; outsl"
 		      : "+S" (__d), "+c"(__n)
 		      : "d"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void insb(unsigned short __p, void *__d, unsigned long __n)
+static inline void insb(unsigned short __p, void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; insb"
 		      : "+D" (__d), "+c"(__n)
 		      : "d"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void insw(unsigned short __p, void *__d, unsigned long __n)
+static inline void insw(unsigned short __p, void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; insw"
 		      : "+D" (__d), "+c"(__n)
 		      : "d"(__p));
 }
 
-#if __STDC_VERSION__ >= 199901L
-inline
-#endif
-static void insl(unsigned short __p, void *__d, unsigned long __n)
+static inline void insl(unsigned short __p, void *__d, unsigned long __n)
 {
 	__asm__ volatile ("cld; rep; insl"
 		      : "+D" (__d), "+c"(__n)
