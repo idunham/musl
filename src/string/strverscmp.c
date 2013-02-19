b219aa373895defe5fba7968eacbd53844145b64
Isaac Dunham <idunham@lavabit.com>
2013-02-18 16:47:06 -0800

    Fixes for strverscmp, take 2.
    This uses size_t instead of int where appropriate, which drags in sys/types.h
    Additionally, change from the more complex while loop
    to a simpler one which exits earlier for equal strings.
    
    More changes to follow.

--- src/string/strverscmp.c
+++ src/string/strverscmp.c
@@ -1,12 +1,15 @@
 #define _GNU_SOURCE
 #include <ctype.h>
 #include <string.h>
+#include <sys/types.h>
 
 int strverscmp(const char *l, const char *r)
 {
 	int haszero=1;
-	while (*l && *r && l[0]==r[0]){
-		if (l[0]=='0'){
+	while (*l==*r) {
+		if (!*l) return 0;
+
+		if (l[0]=='0') {
 			if (haszero==1) {
 				haszero=0;
 			}
@@ -23,7 +26,7 @@ int strverscmp(const char *l, const char *r)
 		haszero=0;
 	}
 	if ((isdigit(l[0]) && isdigit(r[0]) ) && haszero) {
-		int lenl=0, lenr=0, firstl=l[0], firstr=r[0];
+		size_t lenl=0, lenr=0, firstl=l[0], firstr=r[0];
 		while (isdigit(l++[0]) ) {
 			lenl++;
 		}
