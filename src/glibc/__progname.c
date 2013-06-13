351951d43a6d66bb6e7e1b273e4baaab5138fd94
Isaac Dunham <idunham@lavabit.com>
2013-06-12 23:04:10 -0700

    Delete an extra symbol...

--- src/glibc/__progname.c
+++
@@ -1,4 +0,0 @@
-/* this is not intended to be correct, and is only provided for ABI
- * compatibility with truly awful programs */
-const char *__progname = "???";
-const char *__progname_full = "???";
