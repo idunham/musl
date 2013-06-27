7c83ede04a191d58be39ba5d2a5aa0246be6116c
Isaac Dunham <idunham@lavabit.com>
2013-06-26 20:10:45 -0700

    Fix <sys/vm86.h>

--- include/sys/vm86.h
+++ include/sys/vm86.h
@@ -1,7 +1,7 @@
 #ifndef _SYS_VM86_H
 #define _SYS_VM86_H
 
-#include bits/vm86.h
+#include <bits/vm86.h>
 
 int vm86old(struct vm86_struct *info);
 int vm86(unsigned long fn, struct vm86plus_struct *v86);
